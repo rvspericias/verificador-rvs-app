e4322e01d5387d4515abed6defb3a88c871e56ac
rvspericias <carlos@rvspericias.com.br>
2025-05-02 12:25:24 -0300

    Update verificador_rvs_app.py

--- verificador_rvs_app.py
+++ verificador_rvs_app.py
@@ -93,7 +93,7 @@ st.markdown("""
     .header-gold {
         color: #d4af37 !important;
         font-weight: 900;
-        font-size: 2.1rem !important;
+        font-size: 1.8rem !important;
         margin-top: 1em;
         margin-bottom: 1em;
         letter-spacing: 0.01em;
